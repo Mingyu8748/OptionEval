90d1e7e4af19589b27000fbb1e9b91ed57eaaadc
Mingyu8748 <73914578+Mingyu8748@users.noreply.github.com>
2022-12-13 17:29:36 -0600

    update calculator

---
+++ simulator/GreeksCalc.cs
@@ -0,0 +1,290 @@
+namespace Monte_Carlo_Simulation;
+
+public class GreeksCalc
+{
+            public Dictionary<string, double> EuropeanGreeks(Int64 N, double T, double mu, double sigma, double s0, double k,
+            double r, double[,] price, bool iscall, double[,]random_number)
+        {
+            //set up the perturbation first
+            var delta_s = s0 / 100; //10% change
+            var delta_sigma = sigma / 100; //10% change
+            var delta_T = T / 100;
+            var delta_r = r / 100;
+            // Greeks delta calculation
+
+            //stock simulation with perturbations
+            var price_plus_deltas= StockSimulator.StockPath(N, T, mu, sigma, s0+delta_s, random_number);
+            var price_minus_deltas = StockSimulator.StockPath(N, T, mu, sigma, s0-delta_s, random_number);
+            
+            //euoprean option price calculation
+
+            var optionprice = OptionCalculator.EuropeanOptionCalc(N, T, mu, sigma, s0, k, r, price, iscall);
+            var optionprice_plus_deltas = OptionCalculator.EuropeanOptionCalc(N, T, mu, sigma, s0, k, r, price_plus_deltas, iscall);
+            var optionprice_minus_deltas = OptionCalculator.EuropeanOptionCalc(N, T, mu, sigma, s0, k, r, price_minus_deltas, iscall);
+         
+            var delta = (optionprice_plus_deltas - optionprice_minus_deltas) / (2 * delta_s);// Delta      
+            var gamma = (optionprice_plus_deltas - 2 * optionprice + optionprice_minus_deltas) / Math.Pow(delta_s, 2); //gamma
+
+            //vega calculation below 
+            var price_plus_delta_sigma= StockSimulator.StockPath(N, T, mu, sigma+delta_sigma, s0, random_number);
+            var price_minus_delta_simga = StockSimulator.StockPath(N, T, mu, sigma-delta_sigma, s0, random_number);
+
+            var optionprice_plus_sigma = OptionCalculator.EuropeanOptionCalc(N, T, mu, sigma+delta_sigma, s0, k, r, price_plus_delta_sigma, iscall);
+            var optionprice_minus_sigma = OptionCalculator.EuropeanOptionCalc(N, T, mu, sigma-delta_sigma, s0, k, r, price_plus_delta_sigma, iscall);
+        
+            var vega = (optionprice_plus_sigma - optionprice_plus_sigma) / (2 * delta_sigma);
+            
+            //theta calculation below 
+            var price_plus_delta_T= StockSimulator.StockPath(N, T+delta_T, mu, sigma, s0, random_number);
+            var price_minus_delta_T = StockSimulator.StockPath(N, T-delta_T, mu, sigma, s0, random_number);
+
+            var optionprice_plus_deltaT = OptionCalculator.EuropeanOptionCalc(N, T+delta_T, mu, sigma, s0, k, r, price_plus_delta_T, iscall);
+            var optionprice_minus_deltaT = OptionCalculator.EuropeanOptionCalc(N, T-delta_T, mu, sigma, s0, k, r, price_minus_delta_T, iscall);
+        
+            var theta = (optionprice_plus_sigma - optionprice_plus_sigma) / (2 * delta_T);
+            
+            //rho calculation below 
+
+            var optionprice_plus_deltar = OptionCalculator.EuropeanOptionCalc(N, T, mu, sigma, s0, k, r+delta_r, price, iscall);
+            var optionprice_minus_deltar = OptionCalculator.EuropeanOptionCalc(N, T, mu, sigma, s0, k, r-delta_r, price, iscall);
+        
+            var rho = (optionprice_plus_deltar - optionprice_minus_deltar) / (2 * delta_r);
+
+
+            return new Dictionary<string, double>
+            {
+                {"delta", delta},
+                {"gamma", gamma},
+                {"vega", vega},
+                {"rho", rho},
+            };
+        }
+
+
+
+
+public Dictionary<string, double> AisanGreeks(Int64 N, double T, double mu, double sigma, double s0, double k,
+            double r, double[,] price, bool iscall, double[,]random_number)
+        {
+            //set up the perturbation first
+            var delta_s = s0 / 100; //10% change
+            var delta_sigma = sigma / 100; //10% change
+            var delta_T = T / 100;
+            var delta_r = r / 100;
+            // Greeks delta calculation
+
+            //stock simulation with perturbations
+            var price_plus_deltas= StockSimulator.StockPath(N, T, mu, sigma, s0+delta_s, random_number);
+            var price_minus_deltas = StockSimulator.StockPath(N, T, mu, sigma, s0-delta_s, random_number);
+
+            var optionprice = OptionCalculator.AsianOptionCalc(N, T, mu, sigma, s0, k, r, price, iscall);
+            var optionprice_plus_deltas = OptionCalculator.AsianOptionCalc(N, T, mu, sigma, s0, k, r, price_plus_deltas, iscall);
+            var optionprice_minus_deltas = OptionCalculator.AsianOptionCalc(N, T, mu, sigma, s0, k, r, price_minus_deltas, iscall);
+         
+            var delta = (optionprice_plus_deltas - optionprice_minus_deltas) / (2 * delta_s);// Delta      
+            var gamma = (optionprice_plus_deltas - 2 * optionprice + optionprice_minus_deltas) / Math.Pow(delta_s, 2); //gamma
+
+            //vega calculation below 
+            var price_plus_delta_sigma= StockSimulator.StockPath(N, T, mu, sigma+delta_sigma, s0, random_number);
+            var price_minus_delta_simga = StockSimulator.StockPath(N, T, mu, sigma-delta_sigma, s0, random_number);
+
+            var optionprice_plus_sigma = OptionCalculator.AsianOptionCalc(N, T, mu, sigma+delta_sigma, s0, k, r, price_plus_delta_sigma, iscall);
+            var optionprice_minus_sigma = OptionCalculator.AsianOptionCalc(N, T, mu, sigma-delta_sigma, s0, k, r, price_plus_delta_sigma, iscall);
+        
+            var vega = (optionprice_plus_sigma - optionprice_plus_sigma) / (2 * delta_sigma);
+            
+            //theta calculation below 
+            var price_plus_delta_T= StockSimulator.StockPath(N, T+delta_T, mu, sigma, s0, random_number);
+            var price_minus_delta_T = StockSimulator.StockPath(N, T-delta_T, mu, sigma, s0, random_number);
+
+            var optionprice_plus_deltaT = OptionCalculator.AsianOptionCalc(N, T+delta_T, mu, sigma, s0, k, r, price_plus_delta_T, iscall);
+            var optionprice_minus_deltaT = OptionCalculator.AsianOptionCalc(N, T-delta_T, mu, sigma, s0, k, r, price_minus_delta_T, iscall);
+        
+            var theta = (optionprice_plus_sigma - optionprice_plus_sigma) / (2 * delta_T);
+            
+            //rho calculation below 
+
+            var optionprice_plus_deltar = OptionCalculator.AsianOptionCalc(N, T, mu, sigma, s0, k, r+delta_r, price, iscall);
+            var optionprice_minus_deltar = OptionCalculator.AsianOptionCalc(N, T, mu, sigma, s0, k, r-delta_r, price, iscall);
+        
+            var rho = (optionprice_plus_deltar - optionprice_minus_deltar) / (2 * delta_r);
+
+
+            return new Dictionary<string, double>
+            {
+                {"delta", delta},
+                {"gamma", gamma},
+                {"vega", vega},
+                {"rho", rho},
+            };
+        }
+
+public Dictionary<string, double> DigitalGreeks(Int64 N, double T, double mu, double sigma, double s0, double k,
+            double r, double fixed_payoff, double[,] price, bool iscall, double[,]random_number)
+        {
+            //set up the perturbation first
+            var delta_s = s0 / 100; //10% change
+            var delta_sigma = sigma / 100; //10% change
+            var delta_T = T / 100;
+            var delta_r = r / 100;
+            // Greeks delta calculation
+
+            //stock simulation with perturbations
+            var price_plus_deltas= StockSimulator.StockPath(N, T, mu, sigma, s0+delta_s, random_number);
+            var price_minus_deltas = StockSimulator.StockPath(N, T, mu, sigma, s0-delta_s, random_number);
+
+            var optionprice = OptionCalculator.DigitalOptionCalc(N, T, mu, sigma, s0, k, r, fixed_payoff,price, iscall);
+            var optionprice_plus_deltas = OptionCalculator.DigitalOptionCalc(N, T, mu, sigma, s0, k, r, fixed_payoff,price_plus_deltas, iscall);
+            var optionprice_minus_deltas = OptionCalculator.DigitalOptionCalc(N, T, mu, sigma, s0, k, r, fixed_payoff,price_minus_deltas, iscall);
+         
+            var delta = (optionprice_plus_deltas - optionprice_minus_deltas) / (2 * delta_s);// Delta      
+            var gamma = (optionprice_plus_deltas - 2 * optionprice + optionprice_minus_deltas) / Math.Pow(delta_s, 2); //gamma
+
+            //vega calculation below 
+            var price_plus_delta_sigma= StockSimulator.StockPath(N, T, mu, sigma+delta_sigma, s0, random_number);
+            var price_minus_delta_simga = StockSimulator.StockPath(N, T, mu, sigma-delta_sigma, s0, random_number);
+
+            var optionprice_plus_sigma = OptionCalculator.DigitalOptionCalc(N, T, mu, sigma+delta_sigma, s0, k, r, fixed_payoff,price_plus_delta_sigma, iscall);
+            var optionprice_minus_sigma = OptionCalculator.DigitalOptionCalc(N, T, mu, sigma-delta_sigma, s0, k, r, fixed_payoff,price_plus_delta_sigma, iscall);
+        
+            var vega = (optionprice_plus_sigma - optionprice_plus_sigma) / (2 * delta_sigma);
+            
+            //theta calculation below 
+            var price_plus_delta_T= StockSimulator.StockPath(N, T+delta_T, mu, sigma, s0, random_number);
+            var price_minus_delta_T = StockSimulator.StockPath(N, T-delta_T, mu, sigma, s0, random_number);
+
+            var optionprice_plus_deltaT = OptionCalculator.DigitalOptionCalc(N, T+delta_T, mu, sigma, s0, k, r,fixed_payoff, price_plus_delta_T, iscall);
+            var optionprice_minus_deltaT = OptionCalculator.DigitalOptionCalc(N, T-delta_T, mu, sigma, s0, k, r, fixed_payoff,price_minus_delta_T, iscall);
+        
+            var theta = (optionprice_plus_sigma - optionprice_plus_sigma) / (2 * delta_T);
+            
+            //rho calculation below 
+
+            var optionprice_plus_deltar = OptionCalculator.DigitalOptionCalc(N, T, mu, sigma, s0, k, r+delta_r, fixed_payoff,price, iscall);
+            var optionprice_minus_deltar = OptionCalculator.DigitalOptionCalc(N, T, mu, sigma, s0, k, r-delta_r, fixed_payoff,price, iscall);
+        
+            var rho = (optionprice_plus_deltar - optionprice_minus_deltar) / (2 * delta_r);
+
+
+            return new Dictionary<string, double>
+            {
+                {"delta", delta},
+                {"gamma", gamma},
+                {"vega", vega},
+                {"rho", rho},
+            };
+        }
+
+public Dictionary<string, double> LookBackGreeks(Int64 N, double T, double mu, double sigma, double s0, double k,
+            double r, double[,] price, bool iscall, double[,]random_number)
+        {
+            //set up the perturbation first
+            var delta_s = s0 / 100; //10% change
+            var delta_sigma = sigma / 100; //10% change
+            var delta_T = T / 100;
+            var delta_r = r / 100;
+            // Greeks delta calculation
+
+            //stock simulation with perturbations
+            var price_plus_deltas= StockSimulator.StockPath(N, T, mu, sigma, s0+delta_s, random_number);
+            var price_minus_deltas = StockSimulator.StockPath(N, T, mu, sigma, s0-delta_s, random_number);
+
+            var optionprice = OptionCalculator.LookbackOptionCalc(N, T, mu, sigma, s0, k, r, price, iscall);
+            var optionprice_plus_deltas = OptionCalculator.LookbackOptionCalc(N, T, mu, sigma, s0, k, r, price_plus_deltas, iscall);
+            var optionprice_minus_deltas = OptionCalculator.LookbackOptionCalc(N, T, mu, sigma, s0, k, r, price_minus_deltas, iscall);
+         
+            var delta = (optionprice_plus_deltas - optionprice_minus_deltas) / (2 * delta_s);// Delta      
+            var gamma = (optionprice_plus_deltas - 2 * optionprice + optionprice_minus_deltas) / Math.Pow(delta_s, 2); //gamma
+
+            //vega calculation below 
+            var price_plus_delta_sigma= StockSimulator.StockPath(N, T, mu, sigma+delta_sigma, s0, random_number);
+            var price_minus_delta_simga = StockSimulator.StockPath(N, T, mu, sigma-delta_sigma, s0, random_number);
+
+            var optionprice_plus_sigma = OptionCalculator.LookbackOptionCalc(N, T, mu, sigma+delta_sigma, s0, k, r, price_plus_delta_sigma, iscall);
+            var optionprice_minus_sigma = OptionCalculator.LookbackOptionCalc(N, T, mu, sigma-delta_sigma, s0, k, r, price_plus_delta_sigma, iscall);
+        
+            var vega = (optionprice_plus_sigma - optionprice_plus_sigma) / (2 * delta_sigma);
+            
+            //theta calculation below 
+            var price_plus_delta_T= StockSimulator.StockPath(N, T+delta_T, mu, sigma, s0, random_number);
+            var price_minus_delta_T = StockSimulator.StockPath(N, T-delta_T, mu, sigma, s0, random_number);
+
+            var optionprice_plus_deltaT = OptionCalculator.LookbackOptionCalc(N, T+delta_T, mu, sigma, s0, k, r, price_plus_delta_T, iscall);
+            var optionprice_minus_deltaT = OptionCalculator.LookbackOptionCalc(N, T-delta_T, mu, sigma, s0, k, r, price_minus_delta_T, iscall);
+        
+            var theta = (optionprice_plus_sigma - optionprice_plus_sigma) / (2 * delta_T);
+            
+            //rho calculation below 
+
+            var optionprice_plus_deltar = OptionCalculator.LookbackOptionCalc(N, T, mu, sigma, s0, k, r+delta_r, price, iscall);
+            var optionprice_minus_deltar = OptionCalculator.LookbackOptionCalc(N, T, mu, sigma, s0, k, r-delta_r, price, iscall);
+        
+            var rho = (optionprice_plus_deltar - optionprice_minus_deltar) / (2 * delta_r);
+
+
+            return new Dictionary<string, double>
+            {
+                {"delta", delta},
+                {"gamma", gamma},
+                {"vega", vega},
+                {"rho", rho},
+            };
+        }
+        public Dictionary<string, double> RangeGreeks(Int64 N, double T, double mu, double sigma, double s0, double k,
+            double r, double[,] price, double[,]random_number)
+        {
+            //set up the perturbation first
+            var delta_s = s0 / 100; //10% change
+            var delta_sigma = sigma / 100; //10% change
+            var delta_T = T / 100;
+            var delta_r = r / 100;
+            // Greeks delta calculation
+
+            //stock simulation with perturbations
+            var price_plus_deltas= StockSimulator.StockPath(N, T, mu, sigma, s0+delta_s, random_number);
+            var price_minus_deltas = StockSimulator.StockPath(N, T, mu, sigma, s0-delta_s, random_number);
+            
+            //euoprean option price calculation
+
+            var optionprice = OptionCalculator.RangeOptionCalc(N, T, mu, sigma, s0, k, r, price);
+            var optionprice_plus_deltas = OptionCalculator.RangeOptionCalc(N, T, mu, sigma, s0, k, r, price_plus_deltas);
+            var optionprice_minus_deltas = OptionCalculator.RangeOptionCalc(N, T, mu, sigma, s0, k, r, price_minus_deltas);
+         
+            var delta = (optionprice_plus_deltas - optionprice_minus_deltas) / (2 * delta_s);// Delta      
+            var gamma = (optionprice_plus_deltas - 2 * optionprice + optionprice_minus_deltas) / Math.Pow(delta_s, 2); //gamma
+
+            //vega calculation below 
+            var price_plus_delta_sigma= StockSimulator.StockPath(N, T, mu, sigma+delta_sigma, s0, random_number);
+            var price_minus_delta_simga = StockSimulator.StockPath(N, T, mu, sigma-delta_sigma, s0, random_number);
+
+            var optionprice_plus_sigma = OptionCalculator.RangeOptionCalc(N, T, mu, sigma+delta_sigma, s0, k, r, price_plus_delta_sigma);
+            var optionprice_minus_sigma = OptionCalculator.RangeOptionCalc(N, T, mu, sigma-delta_sigma, s0, k, r, price_plus_delta_sigma);
+        
+            var vega = (optionprice_plus_sigma - optionprice_plus_sigma) / (2 * delta_sigma);
+            
+            //theta calculation below 
+            var price_plus_delta_T= StockSimulator.StockPath(N, T+delta_T, mu, sigma, s0, random_number);
+            var price_minus_delta_T = StockSimulator.StockPath(N, T-delta_T, mu, sigma, s0, random_number);
+
+            var optionprice_plus_deltaT = OptionCalculator.RangeOptionCalc(N, T+delta_T, mu, sigma, s0, k, r, price_plus_delta_T);
+            var optionprice_minus_deltaT = OptionCalculator.RangeOptionCalc(N, T-delta_T, mu, sigma, s0, k, r, price_minus_delta_T);
+        
+            var theta = (optionprice_plus_sigma - optionprice_plus_sigma) / (2 * delta_T);
+            
+            //rho calculation below 
+
+            var optionprice_plus_deltar = OptionCalculator.RangeOptionCalc(N, T, mu, sigma, s0, k, r+delta_r, price);
+            var optionprice_minus_deltar = OptionCalculator.RangeOptionCalc(N, T, mu, sigma, s0, k, r-delta_r, price);
+        
+            var rho = (optionprice_plus_deltar - optionprice_minus_deltar) / (2 * delta_r);
+
+
+            return new Dictionary<string, double>
+            {
+                {"delta", delta},
+                {"gamma", gamma},
+                {"vega", vega},
+                {"rho", rho},
+            };
+        }
+}
--- simulator/StockSimulator.cs
+++ simulator/StockSimulator.cs
@@ -2,12 +2,12 @@ namespace Monte_Carlo_Simulation;
 
 class StockSimulator
 {
-    public static double[,] StockPath(Int64 N, double T, double mu, double sigma, double s0, bool VarianceReduction,
-        double[,] inputRandomNumber)
+    public static double[,] StockPath(Int64 N, double T, double mu, double sigma, double s0,
+        double[,] inputRandomNumber) //mu and sigma are assumed annualized values here
     {
         var dailymu = mu / 252;
         var dailysigma = sigma / (Math.Pow(252, 0.5));
-        double[,] stockpriceArray = new double [N, (int) (T * 252) + 1];
+        double[,] stockpriceArray = new double[N, (int)(T * 252) + 1];
         // assign s0 to the first column of simulated stock price metric
         var index = 1;
         while (index <= N)
@@ -16,73 +16,32 @@ class StockSimulator
             index++;
         }
 
-        //simulation for stock price
-        if (VarianceReduction)
+        const double dt = 0.00396825; //set daily timestep simulation
+        var row = 1;
+        while (row <= N)
         {
-            const double dt = 0.00396825; //default dt as 1 day (1/252)
-            var rowpart1 = 1;
-            while (rowpart1 <= (int) N / 2)
+            var col = 2;
+            while (col <= (int)(T * 252) + 1)
             {
-                var colpart1 = 2;
-                while (colpart1 <= (int) (T * 252) + 1)
-                {
-                    stockpriceArray[rowpart1 - 1, colpart1 - 1] =
-                        stockpriceArray[rowpart1 - 1, colpart1 - 2] *
-                        Math.Exp((dailymu - 0.5 * Math.Pow(dailysigma, 2)) * dt +
-                                 sigma * Math.Pow(dt, 0.5) *
-                                 inputRandomNumber[rowpart1 - 1, colpart1 - 2]); //default dt as 1 day (1/252)
-                    colpart1++;
-                }
-
-                rowpart1++;
+                stockpriceArray[row - 1, col - 1] =
+                    stockpriceArray[row - 1, col - 2] *
+                    Math.Exp((mu - 0.5 * Math.Pow(sigma, 2)) * dt +
+                             sigma * Math.Pow(dt, 0.5) *
+                             inputRandomNumber[row - 1, col - 2]);
+                col++;
             }
 
-            var rowpart2 = (int) N / 2;
-            var colpart2 = 2;
-            while (rowpart2 <= N)
-            {
-                while (colpart2 <= (int) (T * 252) + 1)
-                {
-                    stockpriceArray[rowpart2 - 1, colpart2 - 1] =
-                        stockpriceArray[rowpart2 - 1, colpart2 - 2] *
-                        Math.Exp((dailymu - 0.5 * Math.Pow(dailysigma, 2)) * dt -
-                                 sigma * Math.Pow(dt, 0.5) *
-                                 inputRandomNumber[rowpart2 - 1, colpart2 - 2]); //default dt as 1 day (1/252)
-                    colpart2++;
-                }
-
-                rowpart2++;
-            }
+            row++;
         }
-        else
-        {
-            const double dt = 0.00396825; //default dt as 1 day (1/252)
-            var row = 1;
-            while (row <= N)
-            {
-                var col = 2;
-                while (col <= (int) (T * 252) + 1)
-                {
-                    stockpriceArray[row - 1, col - 1] =
-                        stockpriceArray[row - 1, col - 2] *
-                        Math.Exp((mu - 0.5 * Math.Pow(sigma, 2)) * dt +
-                                 sigma * Math.Pow(dt, 0.5) *
-                                 inputRandomNumber[row - 1, col - 2]); //default dt as 1 day (1/252)
-                    col++;
-                }
-
-                row++;
-            }
-        }
-
         return stockpriceArray;
+
     }
 
-    public static void StockPath(object? obj)
-    {
-        var o = (Param) obj!;
-        StockPath(o.n, o.t, o.mu, o.sigma, o.s0, o.varianceReduction, RandomNumberGenerator.Generate(o.n, o.t));
-    }
+public static void StockPath(object? obj)
+{
+    var o = (Param)obj!;
+    StockPath(o.n, o.t, o.mu, o.sigma, o.s0, RandomNumberGenerator.Generate(o.n, o.t));
+}
 }
 
 class Param
